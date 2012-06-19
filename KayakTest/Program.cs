5b466158919402dfe0551c6c5dc74a2d10831ee0
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-06-19 15:54:55 -0600

    Added HTTPListener support

--- KayakTest/Program.cs
+++ KayakTest/Program.cs
@@ -8,12 +8,26 @@ namespace KayakTest
 
     class Program
     {
-        static void Main(string[] args)
+        static void HttpListener(int port)
+        {
+            Console.WriteLine("Listening on port " + port);
+            Console.WriteLine("Press Enter to quit :-)");
+            Gate.Hosts.HttpListener.ServerFactory.Create(Gate.Adapters.Nancy.NancyAdapter.App(), port, "");
+            Console.ReadLine();
+        }
+
+        static void Kayak(int port)
         {
             var ep = new IPEndPoint(IPAddress.Any, 8889);
             Console.WriteLine("Listening on " + ep);
             Console.WriteLine("Press CTRL+C to quit :-)");
             Gate.Hosts.Kayak.KayakGate.Start(new SchedulerDelegate(), ep, Gate.Adapters.Nancy.NancyAdapter.App());
+        }
+
+        static void Main(string[] args)
+        {
+            HttpListener(8889);
+            //Kayak(8889);
         }
     }
 }
